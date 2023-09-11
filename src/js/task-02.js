3e6d32aff255aff8f4c5a68320198752340b2015
BorschVS <borschvadym@gmail.com>
2023-09-11 23:42:46 +0300

    second exaple completed

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const list = ingredients.map((ingredient) => {
+  const listItemEl = document.createElement("li");
+  listItemEl.classList.add("item");
+  listItemEl.textContent = ingredient;
+  return listItemEl;
+});
+
+ingredientsList.append(...list);
+
+console.log(ingredientsList);
