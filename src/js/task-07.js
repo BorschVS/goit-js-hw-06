a9b8b77368feaf4a347910ca61a28c0993e6a09f
BorschVS <borschvadym@gmail.com>
2023-09-16 00:50:29 +0300

    Часткове вирішення 8 завдання

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,11 +1,13 @@
 const input = document.querySelector("#font-size-control");
 const textRef = document.querySelector("#text");
 
+// input.addEventListener("mousemove", onRangeFontSizer);
+
 input.addEventListener("click", onRangeFontSizer);
 input.addEventListener("keydown", onRangeFontSizer);
-
 
 function onRangeFontSizer(event) {
   textRef.style.fontSize = `${event.currentTarget.value}px`;
   console.log(event.currentTarget.value);
+  console.log(event);
 }
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const refs = {
+  form: document.querySelector(".login-form"),
+  email: document.querySelector('[type="email"]'),
+  password: document.querySelector('[type="password"]'),
+  submitBtn: document.querySelector('[type="submit"]'),
+};
+//   console.log(event.currentTarget.elements.password.value);
 
+refs.form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((value, name) => {
+    value.length > 0
+      ? console.log({ name, value })
+      : alert("Всі поля форми повинні бути заповнені!");
+  });
+}
